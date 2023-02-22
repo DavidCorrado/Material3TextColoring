f5425a1f615cb3bd49da9b033834994752449cbd
david.corrado <david.corrado@well.co>
2023-02-22 12:39:54 -0600

    Example

--- app/src/main/java/com/example/textcoloring/MainActivity.kt
+++ app/src/main/java/com/example/textcoloring/MainActivity.kt
@@ -3,44 +3,47 @@ package com.example.textcoloring
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import com.example.textcoloring.ui.theme.TextColoringTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TextColoringTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Column {
+                    Page(MaterialTheme.colorScheme.background)
+                    Page(MaterialTheme.colorScheme.primary)
+                    TextColoringTheme(darkTheme = true) {
+                        Page(MaterialTheme.colorScheme.background)
+                        Page(MaterialTheme.colorScheme.primary)
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TextColoringTheme {
-        Greeting("Android")
+    @Composable
+    fun Page(color: Color) {
+        Surface(color = color) {
+            Column {
+                Text(
+                    text = "Color 1"
+                )
+                Text(
+                    text = "Color 2"
+                )
+                Text(
+                    text = "Color 3"
+                )
+            }
+        }
     }
 }
